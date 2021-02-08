951e04bd91246efe8301289bc6e4e7f9f234541b
Darryl Munro <da.munro@hotmail.com>
2021-02-08 20:41:08 +1300

    Add new Favicon

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <img src={image} alt='View to Paradise' className='absolute object-cover w-full h-full'/>
             <div className='absolute object-cover bg-black opacity-40 w-full h-full'></div>
             <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
-                <h1 className='text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name'>Welcome, my name is Darryl....</h1>
+                <h1 className='text-3xl sm:text-6xl text-white font-bold cursive leading-none lg:leading-snug p-8'>Welcome, my name is Darryl....</h1>
             </section>
         </main>
     )
